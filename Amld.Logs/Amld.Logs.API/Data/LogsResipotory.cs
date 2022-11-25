554832b5564a0c58ebbbfc8e135d3dbc2efccb8d
amwei <amwei@beta.com>
2022-11-25 17:25:35 +0800

    修改

--- Amld.Logs/Amld.Logs.API/Data/LogsResipotory.cs
+++ Amld.Logs/Amld.Logs.API/Data/LogsResipotory.cs
@@ -17,7 +17,25 @@ namespace Amld.Logs.API.Data
 
         public async Task<IEnumerable<LogMessage>> GetMessages(AllReq req)
         {
-           var search = await client.SearchAsync<LogMessage>(x => x.From((req.PageIndex - 1) * req.PageSize));
+            var mustQuerys = new List<Func<QueryContainerDescriptor<LogMessage>, QueryContainer>>();
+            if (!string.IsNullOrEmpty(req.AppId))
+            {
+                mustQuerys.Add(t => t.Term(f => f.AppId, req.AppId));
+            }
+            if (req.LogLevel>0)
+            {
+                mustQuerys.Add(t => t.Term(f => f.LogLevel, req.LogLevel));
+            }
+
+            if (req.StartTime.HasValue && req.EndTime.HasValue)
+            {
+
+            }
+            var search = await client.SearchAsync<LogMessage>(x => x.Query(
+                q => q.Bool(b => b.Must(mustQuerys))
+                ));
+
+           
            return search.Documents;
         }
     }
--- Amld.Logs/Amld.Logs.API/Models/Entities/LogMessage.cs
+++ Amld.Logs/Amld.Logs.API/Models/Entities/LogMessage.cs
@@ -2,5 +2,26 @@
 {
     public class LogMessage
     {
+        /// <summary>
+        /// 应用程序ID
+        /// </summary>
+
+        public string AppId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        public int LogLevel { get; set; }
+
+        /// <summary>
+        /// HTTP Status Code
+        /// </summary>
+        public int HttpStatusCode { get; set; }
+
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+
     }
 }
