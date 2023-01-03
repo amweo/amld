ea5f7ff6d34dfd5f9a2f61f5399aa120263fad76
amwei <amwei@betawm.com>
2023-01-03 10:17:05 +0800

    修改

---
+++ src/Amld.Extensions.Discovery.Consul/ConsulServiceDiscovery.cs
@@ -0,0 +1,10 @@
+﻿namespace Amld.Extensions.Discovery.Consul
+{
+    public class ConsulServiceDiscovery : IServiceDiscovery
+    {
+        public List<string> Address(string serviceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
