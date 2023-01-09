b85bcea49c84cb05eed0cdb46191f4d3f76b1628
amwei <amwei@betawm.com>
2023-01-09 11:40:09 +0800

    修改

---
+++ src/Amld.Platform/Models/LogEntryReq.cs
@@ -0,0 +1,18 @@
+﻿namespace Amld.Platform.Models
+{
+    public class LogEntryReq
+    {
+        /// <summary>
+        /// 应用ID
+        /// </summary>
+        public string AppId { get; set; }
+        /// <summary>
+        /// 链路ID
+        /// </summary>
+        public string ChainId { get; set; }
+        /// <summary>
+        /// 当前TraceId
+        /// </summary>
+        public string TraceId { get; set; }
+    }
+}
---
+++ src/Amld.Platform/Repoitories/LogEntryRepoitory.cs
@@ -0,0 +1,36 @@
+﻿using Amld.Platform.Models;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+
+namespace Amld.Platform.Repoitories
+{
+    public class LogEntryRepoitory
+    {
+        private readonly ElasticsearchClient elasticsearch;
+
+        public LogEntryRepoitory(ElasticsearchClient elasticsearch)
+        {
+            this.elasticsearch = elasticsearch;
+        }
+
+
+        public async Task<bool> Add(LogEntry logEntry)
+        {
+            var c = new QueryDescriptor<LogEntry>();
+
+            if (!string.IsNullOrEmpty(logEntry.AppId))
+            {
+                //var q = new TermQuery(new Field { }) //{ Field = "AppId", Value = logEntry.AppId };
+                //c =  c && new QueryDescriptor<LogEntry>().(f => f.AppId, logEntry.AppId);
+            }
+            var res = await elasticsearch.IndexAsync(logEntry);
+            return res.IsValidResponse;
+        }
+        public (int count, IEnumerable<LogEntry>) Query(LogEntryReq req)
+        {
+            return (0,new List<LogEntry> { });
+        }
+
+        //public LogEntry
+    }
+}
